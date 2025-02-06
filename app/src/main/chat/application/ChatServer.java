4b530d1523a6772a0a5291701782499e9c3c2c04
Ne0Jiku <lovergne.raphael@gmail.com>
2025-02-06 13:26:22 +0100

    Add + connection to server socket

---
+++ app/src/main/chat/application/ChatServer.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(2000);
+
+        System.out.println("Server started. Waiting for client...");
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("Client connected");
+    }
+}
